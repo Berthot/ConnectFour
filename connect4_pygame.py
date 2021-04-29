5cf85b5aa5538052ec27f4fd1f22e83f8d838a1e
allanbraun <allanvobraun@gmail.com>
2021-04-28 21:52:50 -0300

    refatoracoes e checagem de se a coluna esta cheia

--- connect4_pygame.py
+++ connect4_pygame.py
@@ -13,28 +13,55 @@ pygame.init()
 
 x = 492
 y = 490
-screen = pygame.display.set_mode((x,y))
+screen = pygame.display.set_mode((x, y))
 
 squares = []
 
 board_img = pygame.image.load("board.png")
 arrow_img = pygame.image.load("arrow.png")
 
-arrow_direction = 1
+arrow_position = 1
+
 
 def arrow(y):
-    screen.blit(arrow_img,(y,0))
+    screen.blit(arrow_img, (y, 0))
 
-def paint_square(x,y,player):
+
+def paint_square(x, y, player):
     size = 62
     if player == 1:
-        square = pygame.Rect(x,y,size,size)
-        return screen,[255, 0, 0,255],square
+        square = pygame.Rect(x, y, size, size)
+        return screen, [255, 0, 0, 255], square
 
     else:
         square = pygame.Rect(x, y, size, size)
-        return screen, [255,255,0, 255], square
-#pygame.draw.rect(screen, [52, 58, 235, 255], square)
+        return screen, [255, 255, 0, 255], square
+
+
+def check_column_full(position):
+    for row in board:
+        if row[position-1] == 0:
+            return False
+    return True
+
+
+def place_disc():
+    global player
+    if check_column_full(arrow_position):
+        return
+    if player == 1:
+        coordinate = fc.put(board, arrow_position)
+        fc.write_one(board, coordinate)
+        squares.append(paint_square(fcp.arrowhead(arrow_position), fcp.grid_space(coordinate[1]), player))
+        player = 2
+    else:
+        coordinate = fc.put(board, arrow_position)
+        fc.write_two(board, coordinate)
+        squares.append(paint_square(fcp.arrowhead(arrow_position), fcp.grid_space(coordinate[1]), player))
+        player = 1
+
+
+# pygame.draw.rect(screen, [52, 58, 235, 255], square)
 while not winner:
     screen.fill((255, 255, 255))
     for event in pygame.event.get():
@@ -43,29 +70,18 @@ while not winner:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                arrow_direction = fcp.move_LEFT(arrow_direction)
+                arrow_position = fcp.move_LEFT(arrow_position)
 
             if event.key == pygame.K_RIGHT:
-                arrow_direction = fcp.move_RIGHT(arrow_direction)
+                arrow_position = fcp.move_RIGHT(arrow_position)
 
             if event.key == pygame.K_DOWN:
-                if player == 1:
-                    coordinate = fc.put(board,arrow_direction)
-                    fc.write_one(board,coordinate)
-                    squares.append(paint_square(fcp.arrowhead(arrow_direction), fcp.grid_space(coordinate[1]),player))
-                    player = 2
-                else:
-                    coordinate = fc.put(board, arrow_direction)
-                    fc.write_two(board,coordinate)
-                    squares.append(paint_square(fcp.arrowhead(arrow_direction), fcp.grid_space(coordinate[1]), player))
-                    player = 1
+                place_disc()
         winner = fc.verification(board)
-
-
 
     for i in squares:
         pygame.draw.rect(i[0], i[1], i[2])
-    arrow(fcp.arrowhead(arrow_direction))
+    arrow(fcp.arrowhead(arrow_position))
     screen.blit(board_img, (0, 60))
     pygame.display.update()
 print(fc.print_winner(player))
